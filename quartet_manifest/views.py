c5691d0a7d555b40699f526cc78554f853b7a37b
Serial Lab <slab@serial-lab.com>
2018-02-22 10:28:52 -0700

    Cruft removal

--- quartet_manifest/views.py
+++ quartet_manifest/views.py
@@ -1,7 +1,8 @@
 from django.conf import settings
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from rest_framework.views import APIView
+from rest_framework.reverse import reverse
 
 @api_view(['GET'])
 def get_apps(request):
@@ -13,3 +14,4 @@ def get_apps(request):
     apps = [setting for setting in settings.INSTALLED_APPS \
             if 'django.' not in setting]
     return Response(apps)
+
